c0b31516ce2915903e9c5c26ab7efa6f1151237d
dragonprevost <dragon@dkp.io>
2019-01-26 15:09:07 -0800

    made join party comp

---
+++ frontend/src/components/sign_in/join_party.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class JoinParty extends Component{
+	constructor(props){
+			super(props);
+			this.state = {
+					userName: null,
+					userId: null,
+					partyId: null,
+		};
+	}
+	render(){
+		return(<div>sign in page</div>);
+	}
+}
+
+export default JoinParty;
