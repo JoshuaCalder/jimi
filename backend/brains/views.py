d445db8f1b8c3f6fdb62651185362f4de5a105f8
Josh <joshuamcalder@hotmail.com>
2019-01-26 18:17:43 -0800

    create_party functionality added

--- backend/brains/views.py
+++ backend/brains/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import random
+import random, json
 
 from brains.models import Parties
 
-# @param party_adminc
+# @param party_admin
 # @param party_name
 # assigns random 6 digit code to party
 # TODO: add code check, ensuring no duplicates in party codes
 # return 6 digit numeric code
 def create_party(request):
-	party_admin = request.POST.get('party_admin')
-	party_name = request.POST.get('party_name')
+	body = json.loads(request.body)
+	party_admin = body['party_admin']
+	party_name = body['party_name']
 	party_code = random.randint(100000, 999999)
 	Parties.objects.create(
 		party_admin = party_admin,
@@ -25,12 +26,14 @@ def create_party(request):
 # @param user_id
 # registers a user to a party
 def join_party(request):
-	party_id = request.POST.get('party_id')
-	user_id = request.POST.get('user_id')
+	body = json.loads(request.body)
+	party_id = body['party_id']
+	user_id = body['user_id']
 	return HttpResponse('party_id: ' + str(party_id) + ' user_id: ' + str(user_id))
 
 # @param party_id
 # returns list of all top songs associated with a party
 def party_top_tracks(request):
-	party_id = request.POST.get('party_id')
+	body = json.loads(request.body)
+	party_id = body['party_id']
 	return HttpResponse('party_id: ' + str(party_id))
