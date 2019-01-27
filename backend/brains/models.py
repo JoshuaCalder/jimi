d91df74ec5ab19d4762d338807ebf9f9b38553db
Josh <joshuamcalder@hotmail.com>
2019-01-26 16:52:59 -0800

    added create_party endpoint

--- backend/brains/models.py
+++ backend/brains/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Parties(models.Model):
-	party_id = models.IntegerField()
+	party_admin = models.CharField(max_length=100)
+	party_name = models.CharField(max_length=100)
+	party_code = models.IntegerField()
 
 class Users(models.Model):
 	user_id = models.CharField(max_length=100)
--- backend/brains/views.py
+++ backend/brains/views.py
@@ -3,16 +3,23 @@ from django.http import HttpResponse
 
 import random
 
+from brains.models import Parties
+
 # @param user_id
 # @param party_name
 # assigns random 6 digit code to party
 # TODO: add code check, ensuring no duplicates in party codes
 # return 6 digit numeric code
 def create_party(request):
-	user_id = request.POST.get('user_id')
+	party_admin = request.POST.get('party_admin')
 	party_name = request.POST.get('party_name')
 	party_code = random.randint(100000, 999999)
-	return HttpResponse('party_code' + str(party_code))
+	Parties.objects.create(
+		party_admin = party_user,
+		party_name = party_name,
+		party_code = party_code,
+	)
+	return HttpResponse(party_code)
 
 # @param party_id
 # @param user_id
@@ -26,4 +33,4 @@ def join_party(request):
 # returns list of all top songs associated with a party
 def party_top_tracks(request):
 	party_id = request.POST.get('party_id')
-	return HttpResponse('party_id: ' + str(party_id)')
+	return HttpResponse('party_id: ' + str(party_id))
