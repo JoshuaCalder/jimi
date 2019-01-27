fc6071962cb7fc3f594b249256133c1abe36bd78
Josh <joshuamcalder@hotmail.com>
2019-01-26 16:41:34 -0800

    push

--- backend/brains/urls.py
+++ backend/brains/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # urls go here
 urlpatterns = [
+	path('create_party', views.create_party, name='create_party')
 	path('join_party', views.join_party, name='join_party'),
 	path('party_top_tracks', views.party_top_tracks, name='party_top_tracks'),
 ]
--- backend/brains/views.py
+++ backend/brains/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def join_party(request):
-	return HttpResponse('To do')
+import random
 
+# @param user_id
+# @param party_name
+# assigns random 6 digit code to party
+# TODO: add code check, ensuring no duplicates in party codes
+# return 6 digit numeric code
+def create_party(request):
+	user_id = request.POST.get('user_id')
+	party_name = request.POST.get('party_name')
+	party_code = random.randint(100000, 999999)
+	return HttpResponse('party_code' + str(party_code))
+
+# @param party_id
+# @param user_id
+# registers a user to a party
+def join_party(request):
+	party_id = request.POST.get('party_id')
+	user_id = request.POST.get('user_id')
+	return HttpResponse('party_id: ' + str(party_id) + ' user_id: ' + str(user_id))
+
+# @param party_id
+# returns list of all top songs associated with a party
 def party_top_tracks(request):
-	return HttpResponse('To do')
+	party_id = request.POST.get('party_id')
+	return HttpResponse('party_id: ' + str(party_id)')
